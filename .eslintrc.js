1eb7935d61c02554cfeb6ac7f1a3995ca0828245
Ricky Rhodes <ricky@rickyrhodes.com>
2021-03-30 10:20:10 -0400

    chore(configure eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules).reduce(
+  (acc, rule) => {
+    acc[`jsx-a11y/${rule}`] = 'off';
+    return acc;
+  },
+  {}
+);
+
 module.exports = {
   env: {
     browser: true,
@@ -14,9 +22,12 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
+    ...a11yOff,
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/rule-name': 0,
   },
 };
